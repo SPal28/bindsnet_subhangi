d2eeadf1dd3b97aab4d15b6f30abb7b95e8913df
SPal28 <spal412261@gmail.com>
2026-08-03 11:19:32 -0400

    threshold = -56

--- examples/mnist/reservoir.py
+++ examples/mnist/reservoir.py
@@ -83,7 +83,7 @@ network.add_layer(inpt, name="I")
 
 # Reservoir layer - each neurons recieves a voltage, reaches threshold, spike, and resets 
 # thresh represents biologically inspired neurons
-reservoir = LIFNodes(n_neurons, traces=True, thresh = -54 + np.random.randn(n_neurons).astype(float),)
+reservoir = LIFNodes(n_neurons, traces=True, thresh = -56 + np.random.randn(n_neurons).astype(float),)
 network.add_layer(reservoir, name = "R")
 
 # Output layer - creates 10 neurons 
